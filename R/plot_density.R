ea194ed3356142a7d3e098a641fee1bd99e50310
kdybala <kdybala@pointblue.org>
2017-04-18 13:20:06 -0700

    update documentation to include plot_density

--- R/plot_density.R
+++ R/plot_density.R
@@ -15,23 +15,25 @@
 #' \dontrun{
 #' fdat = format_data(data=dat, strata='group')
 #' results = fit_distance_models(fdat, 'speciesname', bins=c(0,10,20,30,40,50,75,100))
-#' plot_density(results, 'D', year=T) + theme_classic() + scale_color_manual(values=c('#682C7','#64AD34'))
+#' plot_density(results, 'D', year=T) + theme_classic() +
+#' scale_color_manual(values=c('#682C7','#64AD34'))
 #' }
 #'
 plot_density = function(obj, type='D', year=F) {
+  Estimate = Label = group = lcl = ucl = xlab = NULL
   if (type %in% names(obj)) {
     dat = obj[[type]]
   } else {
     stop('Requires output of fit_distance_models and type D or N')
   }
   if (year==F) {
-    ggplot2::ggplot(dat, aes(x=Label, y=Estimate, ymin=lcl, ymax=ucl)) + ggplot2::geom_errorbar(width=0.1) +
+    ggplot2::ggplot(dat, ggplot2::aes(x=Label, y=Estimate, ymin=lcl, ymax=ucl)) + ggplot2::geom_errorbar(width=0.1) +
       ggplot2::geom_point()
   } else if (year==T) {
     dat$year = as.numeric(gsub('.*_', '', dat$Label))
     dat$group = as.factor(gsub('_.*', '', dat$Label))
     dat = dat[-which(is.na(dat$year)),]
-    ggplot2::ggplot(dat, aes(x=year, y=Estimate, ymin=lcl, ymax=ucl, color=group, shape=group)) +
+    ggplot2::ggplot(dat, ggplot2::aes(x=year, y=Estimate, ymin=lcl, ymax=ucl, color=group, shape=group)) +
       ggplot2::geom_line() + ggplot2::geom_errorbar(width=0.1) + ggplot2::geom_point() + xlab(NULL)
   }
 }
