bee47e469dc07f7603296e425c4781d232f30717
kdybala <kdybala@pointblue.org>
2017-04-18 11:15:18 -0700

    fix formatting of fit_distance_models

--- R/fit_distance_models.R
+++ R/fit_distance_models.R
@@ -7,15 +7,33 @@
 #' @param modlist List of detection curves to include in analysis. See details.
 #' @param debug Debug level to pass to \code{Distance}. See details.
 #'
-#' @details This function will first subset a formatted point count data set (i.e. output from \code{format_data}) to a desired species and fit a standard list of detection functions to the data using detection bins. An AIC table comparing detection function fits, including Akaike weight (w), whether fit was monotonic (mono), and the p-value from a chi-square goodness-of-fit test (chisq) will then be created and printed. Any models that are monotonic and have a chisq p-value>0.05 will be considered to have "good fit", and marked to be further considered (include = *). Of the included models, the function will plot the results of the one with the lowest AIC score and print density estimates by strata (individuals/ha). Take caution in accepting the goodness-of-fit test results, since the test weights all bins equally even though the first bins are the most important. Check the plot for good agreement between observed and predicted detections. It can be easy to pass the chi-square test with small sample sizes and poor agreement, yet difficult to pass with large sample sizes and very good agreement. If agreement is poor, or none of the models pass the chi-square test, consider lumping bins or reducing the maximum (truncation) distance.
+#' @details This function will first subset a formatted point count data set (i.e. output from \code{format_data}) to a
+#' desired species and fit a standard list of detection functions to the data using detection bins. An AIC table
+#' comparing detection function fits, including Akaike weight (w), whether fit was monotonic (mono), and the p-value
+#' from a chi-square goodness-of-fit test (chisq) will then be created and printed. Any models that are monotonic and
+#' have a chisq p-value>0.05 will be considered to have "good fit", and marked to be further considered (include = *).
+#' Of the included models, the function will plot the results of the one with the lowest AIC score and print density
+#' estimates by strata (individuals/ha). Take caution in accepting the goodness-of-fit test results, since the test
+#' weights all bins equally even though the first bins are the most important. Check the plot for good agreement between
+#' observed and predicted detections. It can be easy to pass the chi-square test with small sample sizes and poor
+#' agreement, yet difficult to pass with large sample sizes and very good agreement. If agreement is poor, or none of
+#' the models pass the chi-square test, consider lumping bins or reducing the maximum (truncation) distance.
 #'
-#' The value for \code{bins} defaults to the cutpoints for CADC protocol VCP25, bins = c(0,10,20,30,40,50,75,100), but cutpoints can be changed for other protocols and/or dropped to lump bins together as needed. Take care in changing cutpoints so that they align with the bins originally used to collect the data, particularly when combining data using more than one protocol. The function will not check whether your bins make sense for your data!
+#' The value for \code{bins} defaults to the cutpoints for CADC protocol VCP25, bins = c(0,10,20,30,40,50,75,100), but
+#' cutpoints can be changed for other protocols and/or dropped to lump bins together as needed. Take care in changing
+#' cutpoints so that they align with the bins originally used to collect the data, particularly when combining data
+#' using more than one protocol. The function will not check whether your bins make sense for your data!
 #'
-#' The default list of detection functions to fit were originally derived from Thomas et al. 2010 J Appl Ecol 47:5-14. These include:
+#' The default list of detection functions to fit were originally derived from Thomas et al. 2010 J Appl Ecol 47:5-14.
+#' These include:
 #' - Uniform key with cosine adjustments (unif.cos)
 #' - Half-normal key with no or cosine adjustments (hn.cos)
 #' - Hazard-rate key with no or polynomial adjustments (hr.poly)
-#' These model names are passed to package \code{Distance}, which produces copious output as each model is running. \code{Distance} will start with no adjustment terms (or the simplest adjustments allowed) and then automatically tries higher order adjustments until it finds one with a higher AIC (poorer fit) than the one before it, and returns the previous model with the lowest AIC. Occasionally, individual models will not fit and will produce an error message, but those will be skipped and the other models will continue to run.
+#' These model names are passed to package \code{Distance}, which produces copious output as each model is running.
+#' \code{Distance} will start with no adjustment terms (or the simplest adjustments allowed) and then automatically
+#' tries higher order adjustments until it finds one with a higher AIC (poorer fit) than the one before it, and returns
+#' the previous model with the lowest AIC. Occasionally, individual models will not fit and will produce an error
+#' message, but those will be skipped and the other models will continue to run.
 #'
 #' Debug level defaults to 0 (none), but can be set to values 1-3 for increasing levels of debugging output.
 #'
@@ -25,9 +43,12 @@
 #'
 #' - \code{table}, containing the table of results printed
 #'
-#' - \code{D}, the density estimates (individuals/ha) for each stratum, calculated from the model with the lowest AIC score (of the models with include = *, see Details)
+#' - \code{D}, the density estimates (individuals/ha) for each stratum, calculated from the model with the lowest AIC
+#' score (of the models with include = *, see Details)
 #'
-#' - \code{N}, the estimated total number of individuals per stratum, calculated from the same model as for \code{D} above, based on extrapolating \code{D} by the \code{Area} of each stratum specified in \code{data}. If \code{Area}=1, then \code{N} = \code{D}.
+#' - \code{N}, the estimated total number of individuals per stratum, calculated from the same model as for \code{D}
+#' above, based on extrapolating \code{D} by the \code{Area} of each stratum specified in \code{data}. If \code{Area}=1,
+#' then \code{N} = \code{D}.
 #'
 #' @author Kristen Dybala, \email{kdybala@@pointblue.org}
 #'
@@ -43,7 +64,8 @@ fit_distance_models = function(data, spec, maxdist=100, bins=c(0,10,20,30,40,50,
 
   ## list of unique surveys
   data$surveyID = paste(data$Region.Label, data$Sample.Label, sep='-')
-  blank <- data[-which(duplicated(data$surveyID)),c('Region.Label','Area','Sample.Label','Effort','count','distance','protocol','surveyID')]
+  blank <- data[-which(duplicated(data$surveyID)),
+                c('Region.Label','Area','Sample.Label','Effort','count','distance','protocol','surveyID')]
   blank$distance = NA
   blank$count = NA
 
@@ -83,15 +105,18 @@ run_models = function(dat, maxdist=100, cuts=NULL, formula=~1, modlist, debug) {
 
   ## uniform with cosine (can't use uniform key with no adjustments)
   if ('unif.cos' %in% modlist) {
-    results$unif.cos = try(Distance::ds(data=dat, transect='point', formula=formula, key='unif', adjustment='cos', cutpoints=cuts, truncation=maxdist, convert.units=0.01, debug.level=debug),TRUE)}
+    results$unif.cos = try(Distance::ds(data=dat, transect='point', formula=formula, key='unif', adjustment='cos',
+                                        cutpoints=cuts, truncation=maxdist, convert.units=0.01, debug.level=debug),TRUE)}
 
   ## half-normal with cosine
   if ('hn.cos' %in% modlist) {
-    results$hn.cos = try(Distance::ds(data=dat, transect='point', formula=formula, key='hn', adjustment='cos', cutpoints=cuts, truncation=maxdist, convert.units=0.01, debug.level=debug),TRUE)}
+    results$hn.cos = try(Distance::ds(data=dat, transect='point', formula=formula, key='hn', adjustment='cos',
+                                      cutpoints=cuts, truncation=maxdist, convert.units=0.01, debug.level=debug),TRUE)}
 
   ## hazard rate with polynomial
   if ('hr.poly' %in% modlist) {
-    results$hr.poly = try(Distance::ds(data=dat, transect='point', formula=formula, key='hr', adjustment='poly', cutpoints=cuts, truncation=maxdist, convert.units=0.01, debug.level=debug),TRUE)}
+    results$hr.poly = try(Distance::ds(data=dat, transect='point', formula=formula, key='hr', adjustment='poly',
+                                       cutpoints=cuts, truncation=maxdist, convert.units=0.01, debug.level=debug),TRUE)}
 
   x=which(lapply(1:length(results), function(x) {class(results[[x]])})=='try-error')
   if (length(x)>0) {results = results[-x]}
